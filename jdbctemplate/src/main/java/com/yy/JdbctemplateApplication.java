6979ae583b42de67e09882756181c565f9ec73fa
12802 <12802>
2022-09-09 09:52:20 +0800

    Jdbctemplate init

---
+++ jdbctemplate/src/main/java/com/yy/JdbctemplateApplication.java
@@ -0,0 +1,14 @@
+package com.yy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+
+@SpringBootApplication
+public class JdbctemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbctemplateApplication.class, args);
+    }
+}
---
+++ jdbctemplate/src/test/java/com/yy/JdbctemplateApplicationTests.java
@@ -0,0 +1,14 @@
+package com.yy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbctemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println(1234);
+    }
+
+}
